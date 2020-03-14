83f9e33a29c3d4f74bed8f52b6a2b612ad8fec9c
glidematrix <mundiamwala@gmail.com>
2020-03-14 23:45:14 +0300

    added dependencies

--- setup.py
+++ setup.py
@@ -13,6 +13,9 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/glidematrix/python-mpg",
     packages=setuptools.find_packages(),
+    install_requires=[
+        'requests',
+    ],
     classifiers=[
         "Mastercard Visa Payment Gateway",
         "License :: OSI Approved :: MIT License",
